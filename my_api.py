178b8cee7aaf5d7428081f1e147f9c3ecb5dd37f
Miles Thomas <mithomtab@gmail.com>
2025-06-27 10:33:45 +0000

    Added SQL database

--- my_api.py
+++ my_api.py
@@ -1,4 +1,7 @@
 import requests
+import sqlalchemy as db
+import pandas as pd
+import json
 
 api_key  = "e3c0d09979ad476686ca082aadf6706c"
 
@@ -11,5 +14,17 @@ params = {
 
 response = requests.get(url, params=params)
 print("Status code:", response.status_code)
-print(response.json())
 
+df = pd.DataFrame.from_dict(response.json())
+df['results_json'] = df['results'].apply(json.dumps)
+df = df.drop(columns=['results'])
+
+engine = db.create_engine('sqlite:///data_base_name.db')
+
+df.to_sql('results', con=engine, if_exists='replace', index=False)
+
+
+
+with engine.connect() as connection:
+   query_result = connection.execute(db.text("SELECT * FROM results;")).fetchall()
+   print(pd.DataFrame(query_result))
